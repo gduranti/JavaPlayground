8ae2e5a2073ee71f77f5cb2433f1f3fcd6e87b92
gabriel-duranti <gabriel-duranti@PRO-PAE-5225.PROCERGS.REDERS>
2019-06-12 12:53:10 -0300

    Cria projeto pixel replacer

---
+++ pixel-replacer/pixel-replacer/src/main/java/com/gduranti/pixelreplacer/Picture.java
@@ -0,0 +1,50 @@
+package com.gduranti.pixelreplacer;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Picture {
+
+    private BufferedImage buf;
+    private int width;
+    private int height;
+
+    public Picture(String name) throws IOException {
+        this.buf = ImageIO.read(new File(name));
+        this.width = buf.getWidth();
+        this.height = buf.getHeight();
+    }
+
+    public Picture(int w, int h) {
+        this.width = w;
+        this.height = h;
+        this.buf = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Color getColor(int col, int row) {
+        return new Color(buf.getRGB(col, row));
+    }
+
+    public void setColor(int col, int row, Color color) {
+        buf.setRGB(col, row, color.getRGB());
+    }
+
+    public File save(String destPath) throws IOException {
+        File tempFile = new File(destPath);
+        ImageIO.write(buf, "png", tempFile);
+        return tempFile;
+    }
+
+}
